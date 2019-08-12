ea29c736db318de1b04cdae30b6aa95ac36adbf1
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2019-08-12 14:42:47 +0300

    add a bunch of comments to the source file

--- sudoku.py
+++ sudoku.py
@@ -7,19 +7,30 @@ import click
 def read_sudoku(file):
     """
     Read from a file the sudoku board.
+
+    :param file: The JSON file containing the sudoku board.
+    :return: The sudoku board.
     """
     with open(file, 'r') as f:
         data = json.load(f)
     return data
 
 def show_sudoku(sudoku, sasudoku=False):
+    """
+    Display the sudoku board as stdout.
+
+    :param sudoku: The sudoku board.
+    :param sasudoku: True if the sudoku param is a SASudoku.state object.
+    False if it's just the object returned by read_sudoku function.
+    :return: Nothing.
+    """
     if sasudoku:
         sudoku = sudoku.state
 
     for row, x in zip(sudoku, range(9)):
 
         if x % 3 == 0 and x > 0 and x < 9:
-            click.echo(21 * "-")    
+            click.echo(21 * "-")
 
         string = ""
         for cell, y in zip(row, range(9)):
@@ -38,8 +49,8 @@ class Cell:
         """
         Initialize a cell.
 
-        value - The value of the cell.
-        variable - Whether the cell's value has to be determined or not.
+        :param value: The value of the cell.
+        :param variable: Whether the cell's value has to be determined or not.
         """
         self.value = value
         self.variable = variable
@@ -49,9 +60,9 @@ class SASudoku:
         """
         Initialize the sudoku by creating a 9x9 grid of cells.
 
-        sudoku - The board's state. None if it's set later on.
-        max_temp - Simulated Annealing's starting temperature.
-        cooldown_rate - Simulated Annealing's cooldown rate.
+        :param sudoku: The board's state. None if it's set later on.
+        :param max_temp: Simulated Annealing's starting temperature.
+        :param cooldown_rate: Simulated Annealing's cooldown rate.
         """
 
         if sudoku:
@@ -160,45 +171,59 @@ def acceptance_probability(energy, new_energy, temperature):
 @click.option('--cooldown-rate', type=click.FLOAT, default=0.5e-5, show_default=True)
 @click.argument('filename')
 def main(initial_temp, stop_temp, cooldown_rate, filename):
+    # read the sudoku board and display it
     state = read_sudoku(filename)
     show_sudoku(state)
 
+    # create a sudoku board object
     sudoku = SASudoku(state)
     best = sudoku
 
+    # premise
     random.seed()
     temperature = initial_temp
     low_temp_limit = stop_temp
     counter = 0
 
+    # run the simulated annealing algorithm until it converges
     while temperature > low_temp_limit:
 
+        # generate neighbour
         neighbour_state = sudoku.get_neighbour()
         neighbour = SASudoku()
         neighbour.state = neighbour_state
 
+        # determine the current and neighbour's energy levels
         energy_old = sudoku.determine_energy()
         energy_new = neighbour.determine_energy()
 
+        # accept the neighbour if it passes the probability function
         random_val = random.uniform(0, 1)
         if acceptance_probability(energy_old, energy_new, temperature) > random_val:
             sudoku = neighbour
 
+        # check if we've set a new record
         if best.determine_energy() > sudoku.determine_energy():
             best = sudoku
 
+        # print the new findings
         best_energy = best.determine_energy()
         current_energy = sudoku.determine_energy()
         if counter % 100 == 0:
             click.echo(f"Best energy: {int(best_energy * 243)}/243 | Current Energy: {int(current_energy * 243)}/243 | Temperature: {temperature}")
+        # and stop it if a solution has been found
         if best_energy == 0.0:
             break
 
+        # otherwise keep on lowering the temperature
         temperature *= (1 - cooldown_rate)
         counter += 1
 
     click.echo("Reached end of simulated annealing")
-    show_sudoku(best, sasudoku=True)
+    if best_energy == 0.0:
+        show_sudoku(best, sasudoku=True)
+    else:
+        click.echo("Haven't found a solution to the sudoku problem")
 
 if __name__ == "__main__":
     main()
